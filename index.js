5a078a1302d289004dc929e2a4301fb20ce9b2e6
Noor banu <noorbanu119@gmail.com>
2025-02-25 11:53:16 +0530

    Added file creation using fs module and async/await

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const fs = require('fs').promises;
+
 const displayMessage = () => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve("Hello, this is a Promise in Node.js!");
         }, 2000);
     });
 };
 
-displayMessage().then(message => console.log(message)).catch(err => console.error(err));
+const createFile = async () => {
+    try {
+        const message = await displayMessage();
+        await fs.writeFile("message.txt", message);
+        console.log("File created successfully with message!");
+    } catch (error) {
+        console.error("Error creating file:", error);
+    }
+};
+
+createFile();
